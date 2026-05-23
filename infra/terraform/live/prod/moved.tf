8fd0c097608e1592d305dc1b128a74cc2e2427b0
Victor Hugo Carvalho Silveira <victorhcs_silveira@hotmail.com>
2026-05-23 19:16:59 -0300

    chore(infra): remover import e moved apos migracao do storage

--- infra/terraform/live/prod/moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = module.storage_backup[0].azurerm_storage_account.backup
-  to   = module.storage.azurerm_storage_account.this
-}
-
-moved {
-  from = module.storage_backup[0].azurerm_storage_container.world_backup
-  to   = module.storage.azurerm_storage_container.world_backups
-}
